4dd120b8c95205212487086e8073e47bd23f3269
jan@appzman.com <jan@appzman.com>
2013-11-18 15:02:13 -0600

    SAP

--- Assignment2-1/SAP.java
+++ Assignment2-1/SAP.java
@@ -1,29 +1,84 @@
 public class SAP{
+private Digraph digraph;
+private boolean[] marked;
+
 // constructor takes a digraph (not necessarily a DAG)     
 public SAP(Digraph G)     
 {
+    digraph = G.copy();
+    marked = new  boolean[G.V()];
 }
 // length of shortest ancestral path between v and w; -1 if no such path     
+
 public int length(int v, int w)      
 {
-return -1;
+    return aLenght(v,v,w,true);
 }
-// a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path     
-public int ancestor(int v, int w)      // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path      
+
+private alength (int s, int v, int w, boolean length)
 {
+    BreadthFirstDirectedPaths bfdp = new BreadthFirstDirectedPaths(digraph, v);
+    if(bfdp.hasPathTo(w)){
+        // get path from s to v and from w to v
+        int l1 = bfdp.distTo(s);
+        int l2 = bfdp.distTo(w);
+        if(length){
+            return l1+l2;
+        }else{
+            return v;
+        }
+    }
+    marked[v] = true;
+    for(int i: digraph.adj(v)){
+        if(!marked[i]){
+           aLength(s,i,w,length);
+        }        
+    }
     return -1;
 }
-// a common ancestor that participates in shortest ancestral path; -1 if no such path   
+
+
+
+
+// a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path     
+public int ancestor(int v, int w)          
+{
+   return aLength(v,v,w,false);
+}
+
+// length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path 
 public int length(Iterable<Integer> v, Iterable<Integer> w)
 {
+    
+   //
+    return -1;
+} 
 
-}  
- public int ancestor(Iterable<Integer> v, Iterable<Integer> w)      // for unit testing of this class (such as the one below)      
+// a common ancestor that participates in shortest ancestral path; -1 if no such path   
+ public int ancestor(Iterable<Integer> v, Iterable<Integer> w)     
  {
- 
+//     Iterable[] it = new Iterable[2];
+//     it[0] = v;
+//     it[1] = w;
+     
+     BreadthFirstDirectedPaths bfdp = new BreadthFirstDirectedPaths(digraph, v);
+     //we need to loop through
+     for(Integer a : w){
+         bfdp.hasPathTo(a.intValue()){
+             //return
+         return bfdp.
+         }
+     }
+     
+  return -1;   
  }
  
  
+ 
+ 
+
+
+// for unit testing of this class (such as the one below)      
 public static void main(String[] args) {
     In in = new In(args[0]);          
     Digraph G = new Digraph(in);          
@@ -35,4 +90,5 @@ public static void main(String[] args) {
         int ancestor = sap.ancestor(v, w);              
         StdOut.printf("length = %d, ancestor = %d\n", length, ancestor); 
     } 
+}
 }
--- Assignment2-1/WordNet.java
+++ Assignment2-1/WordNet.java
@@ -7,17 +7,17 @@ public class WordNet {
         private final HashMap<Integer, Bag<Integer>> hypernyms;
         private final HashMap<String, Bag<Integer>> noun2ids;
         private final HashMap<String, Integer> nounsMap;
+        private final Digraph G;
  private Synset[] synsets;
 // constructor takes the name of the two input files     
-public WordNet(String synsets, String hypernyms)   // the set of nouns (no duplicates), returned as an Iterable  
+public WordNet(String synsets, String _hypernyms)   // the set of nouns (no duplicates), returned as an Iterable  
 {
    In in = new In(synsets);
    String delimiter = ",";
   
-   
    nounsMap =  new  HashMap<String, Integer>();
    id2synset = new  HashMap<Integer, String>();
-   noun2ids =  new HashMap<Integer, Bag<Integer>>();
+   noun2ids =  new HashMap<String, Bag<Integer>>();
    hypernyms =  new HashMap<Integer, Bag<Integer>>();
  
    int count =0;
@@ -34,19 +34,23 @@ public WordNet(String synsets, String hypernyms)   // the set of nouns (no dupli
        String[] nouns = synset.split(" ");
    
        for(String noun : nouns){
-           if(noun2ids.containsKey(noun){
-               Bag b = noun2ids[noun];
-               b.add(Integer(id));
-               noun2ids.put(id,bag);
+           if(noun2ids.containsKey(noun)){
+               Bag b = noun2ids.get(noun);
+               b.add(new Integer(id));
+               noun2ids.put(noun,b);
            }
            else{
                Bag b = new Bag();
-               b.add(Integer(id));
-               noun2ids.put(id,bag);
+               b.add(new Integer(id));
+               noun2ids.put(noun,b);
            }
        }       
    }
    
+   Digraph digraph = new Digraph(id2synset.size());
+   G= digraph;
+   
+   in = new In(_hypernyms);
    //get hypernynms
     while (in.hasNextLine()){
        //parse 
@@ -56,42 +60,59 @@ public WordNet(String synsets, String hypernyms)   // the set of nouns (no dupli
        int id = Integer.parseInt(fields[0]);
        Bag bag = new Bag();
        
-       for( int i=1;i<fields.lenght;i++){
-           bag.add(new Integer(fields[i])); 
+              
+       for( int i=1;i<fields.length;i++){
+           int f = Integer.parseInt(fields[i]);
+           bag.add(new Integer(f)); 
            // I think we should add edges here...
-           digraph.addEdge(id, fields[i]);
+           digraph.addEdge(id, f);
        }
-       hypernyms.put(id,bag);       
+       hypernyms.put(id,bag);
     }
         
 }
 public Iterable<String> nouns()   {
     
     
-    return nounsMap.values();
+    return nounsMap.keySet();
 } 
 // is the word a WordNet noun?    
 public boolean isNoun(String word)  {
 
     
-    return noun2ids.containsKey(noun);
+    return noun2ids.containsKey(word);
 }          
+
 // distance between nounA and nounB (defined below)
 public int distance(String nounA, String nounB)    
 {
     if(!isNoun(nounA)||!isNoun(nounB)){
-        return new java.lang.IllegalArgumentException();
+        throw new java.lang.IllegalArgumentException();
+    }
+    //get vertex id
+ 
+    Bag<Integer> a1 = (Bag<Integer>) noun2ids.get(nounA);
+    Bag<Integer> b1 = (Bag<Integer>) noun2ids.get(nounB);
+    for(Integer a: a1){
+        for(Integer b: b1){
+            int s = a.intValue();
+            int v = b.intValue();
+             DepthFirstDirectedPaths dfdp = new DepthFirstDirectedPaths(G,s);
+                 if(dfdp.hasPathTo(v)){
+                     //return(dfdp.pathTo(v));
+    
+                 }
+        }
     }
     
-    //get distance from A to B
-    
-    return -1;
-}    
+    //return -1;
+ }
+  
 // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB      
 // in a shortest ancestral path (defined below)      
 public String sap(String nounA, String nounB)      
 {
-    
+    //Here we probably need to create a SAP
     
     return "";
 }
